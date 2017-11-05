3d929b4654b1f02b9ab4bf1260c7b3d12bb4cd12
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-05 16:57:11 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -59,6 +59,7 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     artist_name = gets
     selected_artist = Artist.find_by_name(artist_name)
+    binding.pry
     selected_artist.songs.sort! {|song1, song2| song1.name <=> song2.name}
     selected_artist.songs.each_with_index do |song, index|
       puts "#{index + 1}. #{song.description}"
