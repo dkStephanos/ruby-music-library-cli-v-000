cb5b9be7bdf3e26f721f916893f8215506747b06
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-05 17:28:32 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -20,6 +20,7 @@ class MusicLibraryController
     puts("To quit, type 'exit'.")
     puts("What would you like to do?")
     input = gets.chomp
+    binding.pry
     while input != "exit"
       puts("Welcome to your music library!")
       puts("To list all of your songs, enter 'list songs'.")
