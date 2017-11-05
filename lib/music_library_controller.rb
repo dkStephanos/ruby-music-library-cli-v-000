23e875999d6c2b9b6b85aeca488ebefdb8a4057a
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-05 16:29:58 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -35,7 +35,7 @@ class MusicLibraryController
   def list_songs
     Song.all.sort! {|song1, song2| song1.name <=> song2.name}
     Song.all.each_with_index do |song, index|
-      puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      puts "#{index + 1}. #{song.description}"
     end
   end
 
