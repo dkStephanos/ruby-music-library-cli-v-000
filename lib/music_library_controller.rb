d470b7c28eb86a38fa1fb36d9895900ba75dfc96
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-05 15:56:08 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,10 +1,10 @@
 class MusicLibraryController
 
   attr_accessor :importer
-  
-  def initialize(file_path)
+
+  def initialize(file_path = "./db/mp3s")
     @importer = MusicImporter.new(file_path)
-    
+    @importer.import
   end
 
 
