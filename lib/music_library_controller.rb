29a635b84c6b196d4ae38691d5d3bc3da7622c83
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-05 17:23:31 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -30,7 +30,7 @@ class MusicLibraryController
       puts("To play a song, enter 'play song'.")
       puts("To quit, type 'exit'.")
       puts("What would you like to do?")
-      input = gets
+      input = gets.chomp
       case input
       when "list songs"
         list_songs
