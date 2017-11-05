0f7074c972f8371c35f59586964efafd458a1e0c
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-05 15:23:14 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -10,7 +10,7 @@ module Concerns::Findable
   end
 
   def find_or_create_by_name(name)
-    found = Concerns::Findable.find_by_name(name)
+    found = find_by_name(name)
     found ? found : create(name)
   end
 end
