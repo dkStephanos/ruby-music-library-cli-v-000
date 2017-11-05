84de0373b9ce9b8b316a379be4f63cea98d2c3bf
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-05 17:02:00 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -68,7 +68,7 @@ class MusicLibraryController
   end
 
   def list_songs_by_genre
-    puts "Please enter the name of an genre:"
+    puts "Please enter the name of a genre:"
     genre_name = gets
     selected_genre = Genre.find_by_name(genre_name)
     if selected_genre
